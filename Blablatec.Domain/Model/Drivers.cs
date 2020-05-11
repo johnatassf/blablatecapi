62ec95c993dec22ce8faad0b0ab81b9083c5aea4
Johnatas Santos <johnatas.sf@gmail.com>
2020-05-10 23:26:25 -0300

    Adicionando projeto ao repositório

---
+++ Blablatec.Domain/Model/Drivers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blablatec.Domain.Model
+{
+    public class Drivers
+    {
+        public static List<Drivers> MotoristasCadastrados = new List<Drivers> { new Drivers() { Id = 1, Age = 20, Name = "Johnatas" } };
+
+        public Drivers()
+        {
+
+        }
+
+        public decimal Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        
+        //Temporario até ser criado as camadas de repositórios, serviços e data base
+        public List<Drivers> ObterMotoristasCadastrados()
+        {
+            return MotoristasCadastrados;
+        }
+    }
+}
---
+++ Blablatec/Controllers/RegisteredDriversController.cs
@@ -0,0 +1,32 @@
+﻿using Blablatec.Domain.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blablatec.Controllers
+{
+    [ApiController]
+    [Route("motoristas-cadastrados")]
+    public class RegisteredDriversController : ControllerBase
+    {
+        private readonly ILogger<RegisteredDriversController> _logger;
+
+        public RegisteredDriversController(ILogger<RegisteredDriversController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var motoristasCadastrados = new Drivers().ObterMotoristasCadastrados();
+
+            return Ok(motoristasCadastrados);
+        }
+
+     
+    }
+}
