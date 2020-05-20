3095b8b579f7be07137d7cdeaf88a66250bce77e
Rafael Teixeira Miguel <teixeirarafael12@gmail.com>
2020-05-20 19:57:39 -0300

    Mapeamento da entidade de usuário

--- Blablatec.Domain/Model/Usuario.cs
+++ Blablatec.Domain/Model/Usuario.cs
@@ -10,13 +10,20 @@ namespace Blablatec.Domain.Model
     [Table("Usuario")]
     public class Usuario: IEntity
     {
+        [Column("id_usuario")]
         public int Id { get; set; }
+        [Column("nm_email_usuario")]
         public string Email { get; set; }
+        [Column("nm_usuario")]
         public string Nome { get; set; }
+        [Column("nm_sobrenome_usuario")]
         public string Sobrenome { get; set; }
+        [Column("cd_ra_usuario")]
         public string Ra { get; set; }
+        [NotMapped]
         [JsonIgnore]
         public byte[] Passwordhash { get; set; }
+        [NotMapped]
         [JsonIgnore]
         public byte[] Passwordsalt { get; set; }
     }
