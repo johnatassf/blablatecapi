0ea49479d2700b0268e24e6429aab4f0844cdd86
Rafael Teixeira Miguel <teixeirarafael12@gmail.com>
2020-06-04 20:39:18 -0300

    Criação da entidade e controller para Viagem

---
+++ Blablatec.Domain/Model/Viagem.cs
@@ -0,0 +1,31 @@
+﻿using Blablatec.Domain.Interface;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Blablatec.Domain.Model
+{
+    [Table("viagem")]
+    public class Viagem : IEntity
+    {
+        [Column("id_viagem")]
+        public int Id { get; set; }
+        [Column("nm_origem_viagem")]
+        public string Origem { get; set; }
+        [Column("nm_destino_viagem")]
+        public string Destino { get; set; }
+        [Column("qtd_lugar_disponivel")]
+        public string QtdLugar { get; set; }
+        [Column("vl_viagem")]
+        public double Valor { get; set; }
+        [Column("dt_viagem")]
+        public DateTime? Inicio { get; set; }
+        [Column("dt_finalizacao")]
+        public DateTime? Termino { get; set; }
+        [Column("id_usuario_motorista")]
+        public int IdMotorista { get; set; }
+        [ForeignKey("IdMotorista")]
+        public Usuario Motorista { get; set; }
+    }
+}
---
+++ Blablatec/Controllers/ViagemController.cs
@@ -0,0 +1,60 @@
+﻿using Blablatec.Domain.Model;
+using Blablatec.Infra.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blablatec.Controllers
+{
+    [ApiController]
+    [Route("viagem")]
+    public class ViagemController : Controller
+    {
+        private readonly IRepository<Viagem> _repositoryViagem;
+        private readonly IRepository<Usuario> _repositoryUser;
+
+        public ViagemController(
+            IRepository<Viagem> repositoryViagem,
+            IRepository<Usuario> repositoryUser)
+        {
+            _repositoryViagem = repositoryViagem;
+            _repositoryUser = repositoryUser;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var viagems = _repositoryViagem.GetAll();
+
+            viagems.ForEach(p => p.Motorista = _repositoryUser.GetById(p.IdMotorista));
+
+            return Ok(viagems);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById([FromRoute] int id)
+        {
+            var viagem = _repositoryViagem.GetAll();
+
+            if (viagem == null)
+                return NotFound("Viagem {id} não encontrada");
+
+            return Ok(viagem);
+        }
+
+        [HttpPost()]
+        public async Task<IActionResult> CriarViagem(Viagem viagem)
+        {
+            var motorista = await _repositoryUser.GetOne(u => u.Id == viagem.Id);
+
+            if (motorista == null)
+                return BadRequest("Motorista não encontrado");
+
+            viagem = _repositoryViagem.Save(viagem);
+
+            return Created(nameof(GetById), viagem);
+        }
+    }
+}
